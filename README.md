bd8d3355fc20e11b4403587d581532878e6419af
zzinao <kjn0265@gmail.com>
2024-03-15 23:31:40 +0900

    feat: 과제 c 풀이

--- README.md
+++ README.md
@@ -5,3 +5,15 @@
 1. 자바스크립트 기본기 다지기
    1. [프로토타입과 V8엔진 히든클래스 이해하기](./packages/chapter1/src/a.js)
    2. [MicroTask 큐, MacroTask 큐, 애니메이션 등 비동기 메커니즘 이해하기](./packages/chapter1/src/b.js)
+
+### 과제 C
+
+#### 성능 향상 전
+
+<img width="426" alt="스크린샷 2024-03-15 오후 11 16 11" src="https://github.com/zzinao/front1_chap1/assets/77870077/396f62ce-8382-44f0-8d8f-7f1482a84055">
+
+<br />
+
+#### 성능 향상 후
+
+<img width="517" alt="스크린샷 2024-03-15 오후 11 26 55" src="https://github.com/zzinao/front1_chap1/assets/77870077/ed9baaef-2663-4f29-8a42-744330674494">
--- packages/chapter1/src/a.js
+++ packages/chapter1/src/a.js
@@ -18,54 +18,40 @@
  */
 
 // 생성자 함수는 수정하지 마세요
-function Worker(health) {
-  this._health = health ?? 10;
-}
+class Worker {
+  constructor(health = 10) {
+    this._health = health;
+  }
 
-function JuniorEngineer(health, intelligence) {
-  this._super(health);
-  this._intelligence = intelligence ?? 1;
-  if (this._intelligence > 10) {
-    this._isBornGenius = true;
+  getHealth() {
+    return this._health;
+  }
+
+  work() {
+    this._health--;
   }
 }
-//- 생성자 함수는 수정하지 마세요
 
-// 여기에 코드를 작성하세요
-// TO-DO
-//- 여기에 코드를 작성하세요
+class JuniorEngineer extends Worker {
+  constructor(health = 10, intelligence = 1) {
+    super(health);
+    this._intelligence = intelligence;
+    this._isBornGenius = intelligence > 10; // 속성 직접 초기화
+  }
 
-Worker.prototype.getHealth = function () {
-  return this._health;
-};
+  getIntelligence() {
+    return this._intelligence;
+  }
 
-Worker.prototype.work = function () {
-  return this._health--;
-};
+  work() {
+    super.work();
+    this._intelligence++;
+  }
 
-// Object.create를 통해 Worker의 prototype을 JuniorEngineer에게 상속한다.
-// JuniorEngineer.prototype = Object.create(Worker.prototype);
-// JuniorEngineer.prototype.constructor = JuniorEngineer;
-JuniorEngineer.prototype = new Worker();
-
-// 위에서 상속을 받았다 하더라도 prototype에 _super를 명시하지 않으면 생성자 함수에서 _super를 찾을 수 없다.
-JuniorEngineer.prototype._super = function (health) {
-  Worker.call(this, health);
-};
-
-JuniorEngineer.prototype.work = function () {
-  Object.getPrototypeOf(JuniorEngineer.prototype).work.call(this);
-  this._intelligence++;
-};
-
-JuniorEngineer.prototype.getIntelligence = function () {
-  return this._intelligence;
-};
-
-JuniorEngineer.prototype.isBornGenius = function () {
-  return this._isBornGenius ?? false;
-};
-
+  isBornGenius() {
+    return this._isBornGenius;
+  }
+}
 /**
  * ## 문제 A - 추가문제
  *
@@ -83,17 +69,18 @@ JuniorEngineer.prototype.isBornGenius = function () {
  *
  * <제출물>
  * 1. 개선 이전 성능(콘솔화면 캡쳐) // 184.237
- * 2. 개선 이후 성능(콘솔화면 캡쳐)
+ * 2. 개선 이후 성능(콘솔화면 캡쳐) // 68.391834
  * 3. 변경된 코드를 확인할 수 있는 링크 또는 캡쳐 이미지
  *
  * <코멘트>
  * - V8 엔진의 히든클래스 개념을 이해하고 이 개념을 응용하여 최적화 해보세요.
  * - ES 모듈시스템으로 바꾼뒤, 확장자를 .mjs로 변경한 뒤 실행해보세요. 최적화 결과가 같을까요?
  */
+
 function main() {
   var startTime = performance.now();
   for (var i = 0; i < 10000000; i++) {
-    new JuniorEngineer(10, Math.floor(Math.random() * 20)).isBornGenius();
+    new JuniorEngineer(10, (Math.random() * 20) | 0).isBornGenius(); // 정수값 변환하여 성능 향상
   }
   var endTime = performance.now();
 
