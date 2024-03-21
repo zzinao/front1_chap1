4cddd8cbbc51d6088ebd7b5b0247a8facefed686
zzinao <kjn0265@gmail.com>
2024-03-21 23:50:47 +0900

    feat: 과제b useState 구현

--- packages/chapter2/src/hooks.js
+++ packages/chapter2/src/hooks.js
@@ -1,6 +1,30 @@
 export function createHooks(callback) {
+  const global = {};
+  let index = 0;
+
   const useState = (initState) => {
-    return [];
+    if (!global.states) {
+      global.states = [];
+    }
+    const currentState = global.states[index] || initState;
+
+    global.states[index] = currentState;
+
+    const setState = (function () {
+      let currnetIndex = index;
+      return function (value) {
+        if (value !== global.states[currnetIndex]) {
+          resetContext(); // setState 호출 전에 resetContext 실행
+          // 상태 값이 변경되었을 때만 setState 호출
+          global.states[currnetIndex] = value;
+          callback();
+        }
+      };
+    })();
+
+    index = index + 1;
+
+    return [currentState, setState];
   };
 
   const useMemo = (fn, refs) => {
@@ -8,8 +32,8 @@ export function createHooks(callback) {
   };
 
   const resetContext = () => {
-
-  }
+    global.states = [];
+  };
 
   return { useState, useMemo, resetContext };
 }
